d13f88572f20a4702b4ba3b376f841bfd38a8e25
thripthi-3590 <thripthimukkati@gmail.com>
2026-09-06 09:21:42 +0530

    github deploymnet

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Connects to our backend
+    baseURL: 'https://recipe-sharing-platfrom-using-ai-nu-pi.vercel.app/api', // Connects to our backend
 });
 
 // Interceptor to add JWT token to requests if it exists
