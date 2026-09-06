91b55b34065bd1051411a385071ca600510963ab
thripthi-3590 <thripthimukkati@gmail.com>
2026-09-06 09:16:26 +0530

    github deploymnet

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: ["http://localhost:5173", "http://localhost:5174"],
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
